76a2345f4d7d610d945eeb9c02961d4bcf08bfad
Z User <z@container>
2026-05-25 15:30:03 +0000

    65d19267-a601-4879-9f77-53a43a94d210

--- keep-alive.sh
+++ keep-alive.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd /home/z/my-project
 while true; do
-  echo "[$(date)] Starting server..." >> dev.log
-  node --max-old-space-size=4096 node_modules/.bin/next dev -p 3000 >> dev.log 2>&1
-  EXIT_CODE=$?
-  echo "[$(date)] Server exited with code $EXIT_CODE, restarting in 5s..." >> dev.log
+  DATABASE_URL="postgresql://postgres.gybmzmxeknsbypthdvwr:K3las%40pp2025%21DbPass@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres?pgbouncer=true" \
+  DIRECT_URL="postgresql://postgres.gybmzmxeknsbypthdvwr:K3las%40pp2025%21DbPass@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres" \
+  node --max-old-space-size=2048 node_modules/.bin/next dev -p 3000 -H 0.0.0.0 --webpack
+  echo "Server crashed, restarting in 5s..."
   sleep 5
 done
